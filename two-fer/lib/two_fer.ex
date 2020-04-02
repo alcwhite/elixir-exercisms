b612f91e6dc8cdcee2af50a78a038dd9789e96c0
Al White <alcwhite13@gmail.com>
2020-04-02 16:57:47 -0400

    Complete twofer

--- two-fer/lib/two_fer.ex
+++ two-fer/lib/two_fer.ex
@@ -3,6 +3,13 @@ defmodule TwoFer do
   Two-fer or 2-fer is short for two for one. One for you and one for me.
   """
   @spec two_fer(String.t()) :: String.t()
-  def two_fer(name) do
+  def two_fer(name \\ "you") do
+    return_result(name)
+  end
+  defp return_result(name) when is_binary(name) do
+    "One for #{name}, one for me"
+  end
+  defp return_result(_name) do
+    raise FunctionClauseError
   end
 end
